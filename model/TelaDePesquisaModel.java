254b41cc8b4bd8ef823ff9cbba0ad8b3141cdffa
Jaqueline <silvajaqueline9043@gmail.com>
2024-10-14 09:25:36 -0300

    Add files via upload

---
+++ model/TelaDePesquisaModel.java
@@ -0,0 +1,65 @@
+package model;
+import java.sql.*;
+
+import controller.*;
+
+public class TelaDePesquisaModel {
+    public static void pesquisarModel(String textoPesquisa) {
+        try {
+            Connection conexao = MySQLConnector.conectar();
+            String strSqlPesquisa = "select * from `db_senac`.`tbl_senac` where `nome` like '%" + textoPesquisa + "%' or `email` like '%" + textoPesquisa + "%' order by `id` asc;";
+            Statement stmSqlPesquisa = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rstSqlPesquisa = stmSqlPesquisa.executeQuery(strSqlPesquisa);
+            if (rstSqlPesquisa.next()) {
+                rstSqlPesquisa.last();
+                int rowNumbers = rstSqlPesquisa.getRow();
+                rstSqlPesquisa.first();
+
+                stmSqlPesquisa.close();
+                TelaDePesquisaController.notificarUsuario("Legal! Foi(Foram) encontrado(s) " + rowNumbers + " resultado(s).");
+
+                TelaDePesquisaController.preencherCampos(rstSqlPesquisa.getString("id"), rstSqlPesquisa.getString("nome"), rstSqlPesquisa.getString("email"));
+                TelaDePesquisaController.registrarPesquisa();
+
+                TelaDePesquisaController.desabilitarPesquisar();
+                if (rowNumbers > 1) {
+                    TelaDePesquisaController.habilitarAvancar();
+                }
+            } else {
+                TelaDePesquisaController.registarPesquisa();
+                TelaDePesquisaController.desabilitarPesquisar();
+                stmSqlPesquisa.close();
+                TelaDePesquisaController.notificarUsuario("Poxa vida! Não foram encontrados resultados para: \"" + textoPesquisa + "\".");
+            }
+        } catch (Exception e) {
+            System.err.println("Erro: " + e);
+            TelaDePesquisaController.notificarUsuario("Não foi possível prosseguir com a pesquisa! Por favor, verifique e tente novamente.");
+        }
+        
+    } 
+    public static void primeiroRegistroModel(String textoPesquisa) {
+        try {
+            Connection conexao = MySQLConnector.conectar();
+            String strSqlPesquisa = "select * from `db_senac`.`tbl_senac` where `nome` like '%" + textoPesquisa + "%' or `email` like '%" + textoPesquisa + "%' order by `id` asc;";
+            Statement stmSqlPesquisa = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rstSqlPesquisa = stmSqlPesquisa.executeQuery(strSqlPesquisa);
+            if (rstSqlPesquisa.next()) {
+                TelaDePesquisaController.preencherCampos(rstSqlPesquisa.getString("id"), rstSqlPesquisa.getString ("nome"), rstSqlPesquisa.getString ("email"));
+
+                btnProximo.setEnabled(true);
+                btnUltimo.setEnabled(true);
+                TelaDePesquisaController.habilitarAvancar();
+            } else {
+                TelaDePesquisaController.notificarUsuario("Poxa vida! Não foram encontrados resultados para: \"" + textoPesquisa + "\".");
+            }
+            TelaDePesquisaController.registrarPesquisa();
+
+            TelaDePesquisaController.desabilitarPesquisar();
+            stmSqlPesquisa.close();
+        } catch (Exception e) {
+            TelaDePesquisaController.notificarUsuario(textoPesquisa);
+            lblNotificacoes.setText(setHtmlFormat("Não foi possível prosseguir com a pesquisa! Por favor, verifique e tente novamente."));
+            System.err.println("Erro: " + e);
+            }
+        }
+    }
